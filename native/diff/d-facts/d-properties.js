416824339b88f482c67df49ac2c1ace11cb7bc3f
Anton Ovechkin <owanturist@gmail.com>
2017-05-17 14:01:08 +0700

    Made d-properties

---
+++ native/diff/d-facts/d-properties.js
@@ -0,0 +1,62 @@
+/* @flow */
+
+import {
+    type Value,
+    type VPropertiesDict
+} from '../../v-dom/v-facts/v-property';
+
+const resetValue = (value: ?Value): ?string => typeof value === 'string' ? '' : null;
+
+const diffRemove = (prev: VPropertiesDict): ?VPropertiesDict => {
+    let diff: ?VPropertiesDict;
+
+    for (let key in prev) {
+        diff = diff || {};
+        diff[ key ] = resetValue(prev[ key ]);
+    }
+
+    return diff;
+};
+
+const diffUpdate = (prev: VPropertiesDict, next: VPropertiesDict): ?VPropertiesDict => {
+    let diff: ?VPropertiesDict;
+
+    for (let key in prev) {
+        if (key in next) {
+            if (key === 'value' || prev[ key ] !== next[ key ]) {
+                diff = diff || {};
+                diff[ key ] = next[ key ];
+            }
+
+            delete next[ key ];
+        } else {
+            diff = diff || {};
+            diff[ key ] = resetValue(prev[ key ]);
+        }
+    }
+
+    return diff;
+};
+
+const diffCreate = (acc: ?VPropertiesDict, prev: VPropertiesDict, next: VPropertiesDict): ?VPropertiesDict => {
+    for (let key in next) {
+        if (!(key in prev)) {
+            acc = acc || {};
+            acc[ key ] = next[ key ];
+        }
+    }
+
+    return acc;
+};
+
+export const diff = (prev: VPropertiesDict, next: ?VPropertiesDict): ?VPropertiesDict => {
+    if (!next) {
+        return diffRemove(prev);
+    }
+
+    return diffCreate(
+        diffUpdate(prev, next),
+        prev,
+        next
+    );
+};
