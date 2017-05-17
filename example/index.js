76b31f3be6247e6477b1f72de8502453f9f4528f
Anton Ovechkin <owanturist@gmail.com>
2017-05-17 18:06:10 +0700

    Update example by diff calc

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
-import * as App from './App/View';
+import { view } from './App/View';
+import { update } from './App/Update';
 import {
     initialModel
 } from './App/Update';
@@ -8,15 +9,27 @@ import {
 import {
     render
 } from '../native/dom/node';
+import {
+    diff
+} from '../native/diff/d-node';
 
 const hello = (s: string): void => {
+    let model = initialModel;
+    let prevView = view(model);
+
     const foo: any = {
-        tagger: a => a,
+        tagger: a => {
+            model = update(a, model);
+            const nextView = view(model);
+            const df = diff(prevView, nextView);
+            prevView = nextView;
+            console.log(df)
+        },
         parent: undefined
     };
 
     const container: any = document.getElementById('app');
-    const html: any = render(App.view(initialModel), foo);
+    const html: any = render(prevView, foo);
 
     container.appendChild(html);
 }
