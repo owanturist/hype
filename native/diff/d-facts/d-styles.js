617004fe60c3f6b59eaa0f977c41e41e0773909f
Anton Ovechkin <owanturist@gmail.com>
2017-05-17 14:20:46 +0700

    Made d-styles

---
+++ native/diff/d-facts/d-styles.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+import {
+    type VStylesDict
+} from '../../v-dom/v-facts/v-style';
+
+const diffRemove = (prev: VStylesDict): ?VStylesDict => {
+    let diff;
+
+    for (let property in prev) {
+        diff = diff || {};
+        diff[ property ] = '';
+    }
+
+    return diff;
+};
+
+const diffUpdate = (prev: VStylesDict, next: VStylesDict): ?VStylesDict => {
+    let diff;
+
+    for (let property in prev) {
+        diff = diff || {};
+
+        if (property in next) {
+            diff[ property ] = next[ property ];
+            delete next[ property ];
+        } else {
+            diff[ property ] = '';
+        }
+    }
+
+    return diff;
+};
+
+const diffCreate = (acc: ?VStylesDict, next: VStylesDict): ?VStylesDict => {
+    for (let property in next) {
+        acc = acc || {};
+        acc[ property ] = next[ property ];
+    }
+
+    return acc;
+};
+
+const diff = (prev: VStylesDict, next: ?VStylesDict): ?VStylesDict => {
+    if (!next) {
+        return diffRemove(prev);
+    }
+
+    return diffCreate(
+        diffUpdate(prev, next),
+        next
+    );
+};
